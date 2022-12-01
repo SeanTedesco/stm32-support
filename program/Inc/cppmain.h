e33c8c0aaa2ddb5d17d9480898ee31108c35ec4c
Emily <50666053+enext22@users.noreply.github.com>
2022-11-30 22:05:59 -0500

    program: use polling uart receive

--- program/Inc/cppmain.h
+++ program/Inc/cppmain.h
@@ -11,16 +11,16 @@
  extern "C" {
 #endif
 
+
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 
 
 extern I2C_HandleTypeDef hi2c1;		// i2c connection to RPi
 extern SPI_HandleTypeDef hspi3;		// spi connection to RPi
 extern UART_HandleTypeDef huart1;	// serial connection to RPi
 extern UART_HandleTypeDef huart2;	// serial connection to host PC
-
-
 
 
 void cpp_main(void);
--- program/Src/cppmain.cpp
+++ program/Src/cppmain.cpp
@@ -6,6 +6,8 @@
 
 #include <cppmain.h>
 #include <uart_protocol.h>
+
+
 int some_variable = 0;
 
 void foo(int input){
@@ -16,13 +18,20 @@ void foo(int input){
 void cpp_main(){
 	// do stuff once
 
+	//uint8_t buffer[32];
+	uint8_t obc_buffer[32];
+
 	// main loop
 	while(1){
 		// do stuff repeatedly
 
-		print_string(huart2, "hello qset!!\r\n");
-
 		print_string(huart1, "hello obc!!\r\n");
+
+		uart_rx_byte_singlestring(huart1, obc_buffer);
+
+		print_string(huart2, "received from obc: ");
+		print_string(huart2, (char*)obc_buffer);
+		print_string(huart2, "\r\n");
 
 		HAL_Delay(1000);
 
