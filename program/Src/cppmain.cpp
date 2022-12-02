4f508ff161dd8a67b03b1bf567d3d7221d1ad3f3
Emily <50666053+enext22@users.noreply.github.com>
2022-12-01 19:58:05 -0500

    program: src: main: use interupt to handle uart communication

--- program/Src/cppmain.cpp
+++ program/Src/cppmain.cpp
@@ -4,36 +4,44 @@
  * @date	2022-11-29
  */
 
+/*********************************************************************************
+ * INCLUDES
+ */
 #include <cppmain.h>
 #include <uart_protocol.h>
+#include <i2c_protocol.h>
 
+/*********************************************************************************
+ * LOCAL VARIABLES
+ */
+uint8_t tx_buffer[] = "test, test, test!";
+uint8_t rx_buffer[32];
+uint8_t rx_byte;
+uint8_t rx_flag;
+uint16_t rxldx;
 
-int some_variable = 0;
-
-void foo(int input){
-  // do something
+/*********************************************************************************
+ * LOCAL FUNCTIONS
+ */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	HAL_UART_Receive_IT(&huart1, rx_buffer, 12);
 }
 
-
+// do stuff once
 void cpp_main(){
-	// do stuff once
 
-	//uint8_t buffer[32];
-	uint8_t obc_buffer[32];
+	HAL_UART_Receive_IT(&huart1, rx_buffer, 12);
 
-	// main loop
+	// main loop, do stuff repeatedly
 	while(1){
-		// do stuff repeatedly
-
-		print_string(huart1, "hello obc!!\r\n");
-
-		uart_rx_byte_singlestring(huart1, obc_buffer);
 
 		print_string(huart2, "received from obc: ");
-		print_string(huart2, (char*)obc_buffer);
+		print_string(huart2, (char*)rx_buffer);
 		print_string(huart2, "\r\n");
 
-		HAL_Delay(1000);
+		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+		HAL_Delay(250);
 
 	}
 }
