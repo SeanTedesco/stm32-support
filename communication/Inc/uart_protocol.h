d596da875fd1af5aa302aa166bd0ed7077960b1c
Sean Tedesco <stedesco@live.com>
2022-12-19 16:40:15 -0500

    comm: inc: uart: provide global rx controls

--- communication/Inc/uart_protocol.h
+++ communication/Inc/uart_protocol.h
@@ -7,13 +7,19 @@
 #ifndef UART_PROTOCOL_H_
 #define UART_PROTOCOL_H_
 
+#define max_rx_buffer_size 128
+
+extern uint8_t uart1_rx_buffer[max_rx_buffer_size];
+extern uint8_t uart1_rx_byte;
+extern uint8_t uart1_rx_flag;
+
+extern uint8_t uart2_rx_buffer[max_rx_buffer_size];
+extern uint8_t uart2_rx_byte;
+extern uint8_t uart2_rx_flag;
+
 class Serial {
 	public: // attributes
 		UART_HandleTypeDef internal_huart;
-		uint16_t rxidx = 0;
-		uint8_t rx_flag = 0;
-		uint8_t rx_buffer[32];
-		uint8_t rx_byte;
 
 	public: // methods
 		Serial(UART_HandleTypeDef huart);
