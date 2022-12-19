0eb32a98989d837dd3c48415bf552b99bff6213b
Sean Tedesco <stedesco@live.com>
2022-12-19 16:44:41 -0500

    program: src: remove deprecated uart callback

--- program/Src/cppmain.cpp
+++ program/Src/cppmain.cpp
@@ -15,53 +15,33 @@
 /*********************************************************************************
  * LOCAL VARIABLES
  */
-uint8_t tx_buffer[] = "test, test, test!";
-uint16_t rxidx = 0;
-uint8_t rx_flag = 0;
-uint8_t rx_buffer[32];
-uint8_t rx_byte;
-
 uint8_t RxAddress[] = {0xEE,0xDD,0xCC,0xBB,0xAA}; // list of data pipes available
 uint8_t RxData[32];
 uint8_t txData[32];
 uint8_t TxAddress[] = {0xEE,0xDD,0xCC,0xBB,0xAA};
-uint8_t uButtonFlag = 0;
-
 
 /*********************************************************************************
  * LOCAL FUNCTIONS
  */
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-	if(huart->Instance == USART1){
-		if (rx_byte == '>' || rxidx >= (32-1)) {
-			rx_buffer[rxidx] = rx_byte;
-			rx_flag = 1;
-			rxidx = 0;
-		} else {
-			rx_buffer[rxidx] = rx_byte;
-			rxidx++;
-		}
-	}
-	HAL_UART_Receive_IT(&huart1, &rx_byte, 1);
-}
+
 
 // do stuff once
 void cpp_main(){
 
-	Serial myserial(huart2);
-
-	HAL_UART_Receive_IT(&huart1, &rx_byte, 1);
+	Serial transmitter(huart2);
+	HAL_UART_Receive_IT(&huart1, &uart1_rx_byte, 1);	// receive on huart1
 
 	// main loop, do stuff repeatedly
 	while(1){
 
-		myserial.print_string("received from obc: ");
-		myserial.print_string((char*)rx_buffer);
-		myserial.print_string("\r\n");
+		if (uart1_rx_flag == 1){
+			uart1_rx_flag = 0;
+			transmitter.print_string("received from obc: ");
+			transmitter.print_string((char*)uart1_rx_buffer);
+			transmitter.print_string("\r\n");
+		}
 
 		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
 		HAL_Delay(250);
-
 	}
 }
